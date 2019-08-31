d25f779f70837b9f289173b19440cf7e943c5a0a
SRGTxTwinkie <54576833+SRGTxTwinkie@users.noreply.github.com>
2019-08-31 19:37:42 -0400

    Update Universal Shortcut (Mouse) V-1.00.py

--- Universal Shortcut (Mouse) V-1.00.py
+++ Universal Shortcut (Mouse) V-1.00.py
@@ -1,38 +1,34 @@
-import subprocess, pyautogui, os
+import subprocess, pyautogui, os, time
 import tkinter as tk
 
 
 class values:
-    x = ''
-    top = tk.Tk()
-    var = tk.StringVar()
-    realWebsite = True
-    E1 = ''
-    custom = False
-    var = tk.StringVar()
-    ip = 0
-    suffix = ''
+    x = '' ### User URL
+    top = tk.Tk() ### IDK why I named it top, but it's shorter than tk.TK()
+    var = tk.StringVar() ### Converts the Custom Text Box to A string that Python can Interpret
+    realWebsite = True ### This is the variable that controls whether the browser opens, Boolean cuz I hate myself
+    E1 = '' ### This is What the user Enters in the custom text box
+    custom = False ### Controls whether the custom() function is active
+    ip = 0 ### Can either be 1 or 0, depending on what the ping() function returns
+    suffix = '' ### Controls the ending of the URL ie: .com,.org, shit like that
 
-def ping():
-    print(values.x)
+def ping(): ### Pings Website that the user types in to make sure it a real website, my god this looks like a mess
     values.ip = os.system('ping www.' + values.x + '.com -n 1 >>text.txt') ### Uses bach to ping website to make sure it exists
-    if values.ip == 1:
-        values.ip = os.system('ping www.' + values.x + '.org -n 1 >>text.txt')
+    if values.ip == 1: 
+        values.ip = os.system('ping www.' + values.x + '.org -n 1 >>text.txt') 
         if values.ip == 1:
             values.realWebsite = False
-            print('22')
             return
-        else:
+        else: ### Controls Suffix, may add more in the future, but I doubt it
             values.suffix = '.org'
             values.realWebsite = True
             return
     else:
-        print('30')
         values.realWebsite = True
         return
         
 
-def windowOpen():
+def windowOpen(): ### Opens the internet browswer of choice, FireFox is better
     try:
         for y in range(5): ### Retries 5 times because sometimes it errors out for permissions 
             try:
@@ -54,7 +50,7 @@ def windowOpen():
     except:
         print('an error has occured')### Ideally this should never appear, because of the other error checking. But that remains to be seen
 
-def reddit():
+def reddit(): ###This and Youtube are the same, customString() is the only one that is different 
     values.x = 'old.reddit'
     ping()
     if values.realWebsite == False:
@@ -62,6 +58,7 @@ def reddit():
         return
     else:
         windowOpen()
+        time.sleep(.5)
         pyautogui.typewrite('https://' + 'old.reddit' + '.com')
         pyautogui.press('enter')
         print('...Done')
@@ -74,12 +71,13 @@ def youtube():
         return
     else:
         windowOpen()
+        time.sleep(.5)
         pyautogui.typewrite('https://' + 'youtube' + '.com')
         pyautogui.press('enter')
         print('...Done')
 
-def custom():
-    L1 = tk.Label(values.top, text="Gimme dat link: ")
+def custom(): ### Oh doggy, does this control the custom URL
+    L1 = tk.Label(values.top, text="Gimme dat link: ") ### This is the Label for the Text Box, Kinda unessicary
     L1.pack()
     values.E1 = tk.Entry(values.top, textvariable = values.var)
     values.E1.pack()
@@ -97,6 +95,7 @@ def customString():
             print('Website Unavailible')
             return
         windowOpen()
+        time.sleep(.5)
         pyautogui.typewrite('https://' + values.x + values.suffix)
         pyautogui.press('enter')
         print('...Done')
